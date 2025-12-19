3b15b11febbaf4b313c2a4933f1048e52aee2ed7
iamrautela <abhisheksinghrautela036@gmail.com>
2025-12-19 18:45:57 +0530

    Update backend configuration: set DB_URL in .env, refactor server.js to use startServer function, and implement connectDB in db.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
-import {ENV} from "./lib/env.js";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 
 const app = express()
@@ -9,7 +10,21 @@ console.log(ENV.PORT);
 console.log(ENV.DB_URL);
 
 app.get("/health", (req, res) => {
-    res.status(200).json({msg: "api is up and running"});
+    res.status(200).json({ msg: "api is up and running" });
 })
 
-app.listen(ENV.PORT, () => console.log("Server running on port :", ENV.PORT));
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log("server is running on port:", ENV.PORT);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
