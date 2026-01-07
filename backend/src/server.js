046f81291b61c6dbb21e8b86d136a5874db3fb62
iamabhirautela <abhisheksinghrautela036@gmail.com>
2026-01-07 18:47:36 +0530

    feat: update backend configuration with MongoDB connection and refactor server initialization

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 
 const app = express();
@@ -10,9 +11,21 @@ console.log(process.env.PORT);
 console.log(process.env.DB_URL);
 
 
-app.get("/", (req, res) => {
-    res.status(200).json({msg: "success for project 124"})
+app.get("/health", (req, res) => {
+    res.status(200).json({ msg: "api is up and running" });
 })
 
-app.listen(ENV.PORT, () => {
-    console.log("Server is running on port:", ENV.PORT)});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log(`Server is running on port ${ENV.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Error starting the server:", error);
+
+    }
+}
+startServer();
